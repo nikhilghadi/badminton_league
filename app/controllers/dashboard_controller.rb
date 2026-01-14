1ea16c42f4bc4bd7895b77a392621b9f1709c33b
nikhilghadi <nikhilghadi665@gmail.com>
2026-01-14 12:58:32 +0530

    leaderboard query optimised and resolve N+1 problem

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,27 @@
 class DashboardController < ApplicationController
   def index
-    @players = Player
-      .left_joins(:wins)
-      .select('players.*, COUNT(matches.id) AS wins')
-      .group('players.id')
-      .order('wins DESC')
+    sql = <<~SQL
+      SELECT players.*,
+            ifnull(winning_stats.wins, 0)  AS wins_count,
+            ifnull(losing_stats.losses, 0) AS losses_count
+      FROM players
+      LEFT JOIN (
+        SELECT winner_id, COUNT(*) AS wins
+        FROM matches
+        GROUP BY winner_id
+      ) winning_stats
+        ON winning_stats.winner_id = players.id
+      LEFT JOIN (
+        SELECT loser_id, COUNT(*) AS losses
+        FROM matches
+        GROUP BY loser_id
+      ) losing_stats
+        ON losing_stats.loser_id = players.id
+      ORDER BY wins_count DESC
+    SQL
+
+  @players = Player.find_by_sql(sql)
+
+
   end
 end
