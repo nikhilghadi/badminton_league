43203c588dc0d140109acb7c0d5511e207cf815d
nikhilghadi <nikhilghadi665@gmail.com>
2026-01-14 10:55:14 +0530

    leader board added

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class DashboardController < ApplicationController
   def index
-
+    @players = Player
+      .left_joins(:wins)
+      .select('players.*, COUNT(matches.id) AS wins')
+      .group('players.id')
+      .order('wins DESC')
   end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,8 @@
 class Player < ApplicationRecord
+  has_many :wins, class_name: 'Match', foreign_key: 'winner_id'
+  has_many :losses, class_name: 'Match', foreign_key: 'loser_id'
+
+  def matches
+    Match.where('winner_id = ? OR loser_id = ?', id, id)
+  end
 end
